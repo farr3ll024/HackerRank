3718eb577924a57dfc2867d305ef97f18ba59e22
Blaise Reints <blaise@remyhealth.com>
2019-09-10 06:58:00 -0500

    Day 1 challenge: Data Types

--- Day30Challenge/Day1_DataTypes.py
+++ Day30Challenge/Day1_DataTypes.py
@@ -1,15 +1,31 @@
-# Sample input
-i = 4
-d = 4.0
-s = 'HackerRank '
-# Declare second integer, double, and String variables.
+def data_types():
+    i = 4
+    d = 4.0
+    s = 'HackerRank '
+    default_parms = [i, d, s]
+
+    parms = get_input()
+    run_ops(parms, default_parms)
+
+def get_input():
+    input_params = []
+    for x in range(3):
+        input_params.append(input())
+    return input_params
+
+def run_ops(parms, default_parms):
+    i = 0
+    for parm, default_parm in zip(parms, default_parms):
+        if i == 0:
+            print(int(parm) + default_parm)
+            i += 1
+            continue
+        elif i == 1:
+            print(float(parm) + default_parm)
+            i += 1
+        else:
+            print(default_parm + str(parm))
+            i += 1
+            continue
 
 
-# Read and save an integer, double, and String to your variables.
-
-# Print the sum of both integer variables on a new line.
-
-# Print the sum of the double variables on a new line.
-
-# Concatenate and print the String variables on a new line
-# The 's' variable above should be printed first.
--- Day30Challenge/main.py
+++ Day30Challenge/main.py
@@ -1,9 +1,9 @@
 from Day0_HelloWorld import hello_world
-
+from Day1_DataTypes import data_types
 
 def main():
-    hello_world()
-
+#     hello_world()
+    data_types()
 
 if __name__ == "__main__":
     main()
