a0eec80d5550288bd8214ecffcb7d8bf6fac4bd9
Blaise Reints <blaise@remyhealth.com>
2019-09-10 08:16:33 -0500

    Day 3: conditional statements

---
+++ Day30Challenge/Day3_ConditionalStatements.py
@@ -0,0 +1,7 @@
+def conditional_statements():
+    n = int(input())
+
+    if n % 2 == 0 and (n in range(2, 5) or n > 20):
+        print("Not Weird")
+    else:
+        print("Weird")
--- Day30Challenge/main.py
+++ Day30Challenge/main.py
@@ -1,11 +1,13 @@
 from Day0_HelloWorld import hello_world
 from Day1_DataTypes import data_types
 from Day2_Operators import operators
+from Day3_ConditionalStatements import conditional_statements
 
 def main():
 #     hello_world()
 #     data_types()
-    operators()
+#     operators()
+    conditional_statements()
 
 if __name__ == "__main__":
     main()
